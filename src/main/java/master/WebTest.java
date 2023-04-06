584f6e56b8d324f1e52c3a09ed935f5b1eacc050
Zura <jur.and.jr42@gmail.com>
2023-04-06 09:40:56 -0300

    Adding positiveLoginTest

--- src/main/java/master/WebTest.java
+++ src/main/java/master/WebTest.java
@@ -2,8 +2,13 @@ package master;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WebTest {
     @BeforeAll
@@ -11,8 +16,21 @@ public class WebTest {
         System.setProperty("webdriver.edge.driver", "C:\\Users\\juran\\Downloads\\Selenium WebDrivers\\edgedriver_win64\\msedgedriver.exe");
     }
     @Test
-    public void login() {
+    public void positiveLoginTest() {
         WebDriver driver = new EdgeDriver();
         driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        WebElement usernameEditBox = driver.findElement(By.name("username"));
+        WebElement passwordEditBox = driver.findElement(By.name("password"));
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        boolean succesfulMessage = driver.getPageSource().contains("Congratulations");
+
+        usernameEditBox.sendKeys("student");
+        passwordEditBox.sendKeys("Password123");
+
+        submitButton.click();
+
+        assertTrue(succesfulMessage);
     }
+
 }
