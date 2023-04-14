7bbbe6aa68b31f73f8f106260478a5c5ab305df7
Zura <jur.and.jr42@gmail.com>
2023-04-14 15:20:30 -0300

    Negative username test added

--- src/main/java/master/WebTest.java
+++ src/main/java/master/WebTest.java
@@ -14,6 +14,7 @@ public class WebTest {
     @BeforeAll
     public static void init() {
         System.setProperty("webdriver.edge.driver", "C:\\Users\\juran\\Downloads\\Selenium WebDrivers\\edgedriver_win64\\msedgedriver.exe");
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
     }
     @Test
     public void positiveLoginTest() {
@@ -31,6 +32,25 @@ public class WebTest {
         submitButton.click();
 
         assertTrue(succesfulMessage);
+        driver.close();
     }
 
+    @Test
+    public void negativeUsernameTest() {
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        WebElement usernameEditBox = driver.findElement(By.name("username"));
+        WebElement passwordEditBox = driver.findElement(By.name("password"));
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        boolean errorMessage = driver.getPageSource().contains("Your username is invalid!");
+
+        usernameEditBox.sendKeys("incorrectUser");
+        passwordEditBox.sendKeys("Password123");
+
+        submitButton.click();
+
+        assertTrue(errorMessage);
+        driver.close();
+    }
 }
